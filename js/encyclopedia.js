d0e9459bb6ac05e611a4f5ff826805219d3c2fd3
SO-SO <sys107502@gmail.com>
2026-05-26 17:32:39 +0900

    Feat: DB와 도감 연동

--- js/encyclopedia.js
+++ js/encyclopedia.js
@@ -1,36 +1,65 @@
-// 더미데이터 monsters를 받아옴
-import monsters from "/js/monsters.js";
+const API = "http://localhost:8080/api/monster/list";
 
 const pageSize = 6;
-//페이지 네이션
+/* 현재 페이지 */
 let currentPage = 1;
-renderPage(currentPage);
-renderPagination();
+/* 전체 몬스터 저장 */
+let allMonsters = [];
+/* 최초 실행 */
+loadMonsters();
+// 몬스터 조회
+function loadMonsters() {
+    fetch(API)
+        .then((res) => res.json())
 
-//몬스터 카드 함수
+        .then((data) => {
+            console.log("몬스터 데이터:", data);
+
+            allMonsters = data.data;
+
+            renderPage(currentPage);
+
+            renderPagination();
+        })
+
+        .catch((err) => {
+            console.error("몬스터 불러오기 실패:", err);
+        });
+}
+// 몬스터 카드 생성
 function appendMonsterCards(monsters) {
     const grid = document.getElementById("wikiGrid");
 
     monsters.forEach((monster) => {
+        console.log(monster);
         const card = document.createElement("div");
-        card.className = "monster-card";
-        //카드 형태 정의
-        card.innerHTML = `
-      <div class="img-area">
-        <img
-          src="${monster.image}"
-          alt="${monster.name}"
-        >
-      </div>
-      <div class="monster-info">
-        <div class="difficulty">
-          ${monster.difficulty}
-        </div>
-        <h3>${monster.name}</h3>
-      </div>
 
-    `;
-        //카드 클릭시 모달 오픈
+        card.className = "monster-card";
+
+        card.innerHTML = `
+
+            <div class="img-area">
+
+                <img
+                    src="${monster.image}"
+                    alt="${monster.name}"
+                >
+
+            </div>
+
+            <div class="monster-info">
+
+                <div class="difficulty">
+                    ${monster.level}
+                </div>
+
+                <h3>${monster.name}</h3>
+
+            </div>
+
+        `;
+
+        /* 카드 클릭 시 모달 오픈 */
         card.addEventListener("click", () => {
             openModal(monster);
         });
@@ -38,63 +67,106 @@ function appendMonsterCards(monsters) {
         grid.appendChild(card);
     });
 }
-//페이지 렌더링
+// 페이지 렌더링
 function renderPage(page) {
     const grid = document.getElementById("wikiGrid");
+
     grid.innerHTML = "";
+
     const start = (page - 1) * pageSize;
+
     const end = start + pageSize;
-    const currentMonsters = monsters.slice(start, end);
+
+    const currentMonsters = allMonsters.slice(start, end);
+
     appendMonsterCards(currentMonsters);
 }
-//페이지 네이션 렌더링
+// 페이지네이션 렌더링
 function renderPagination() {
     const pagination = document.getElementById("pagination");
-    const totalPages = Math.ceil(monsters.length / pageSize);
+
+    pagination.innerHTML = "";
+
+    const totalPages = Math.ceil(allMonsters.length / pageSize);
+
     for (let i = 1; i <= totalPages; i++) {
         const btn = document.createElement("button");
+
         btn.textContent = i;
+
+        /* 현재 페이지 active */
+        if (i === currentPage) {
+            btn.classList.add("active");
+        }
+
         btn.addEventListener("click", () => {
             currentPage = i;
+
             renderPage(currentPage);
+
+            renderPagination();
         });
+
         pagination.appendChild(btn);
     }
 }
-//모달 오픈 함수
+// 모달 오픈
 function openModal(monster) {
     const modal = document.getElementById("wikiModal");
-    //모달 내용 정의
+
     modal.innerHTML = `
+
         <div class="modal-box">
+
             <button class="close-btn">
                 X
             </button>
+
             <img
                 src="${monster.image}"
                 alt="${monster.name}"
                 class="modal-img"
             >
+
             <h2>${monster.name}</h2>
+
             <p>
                 난이도 :
-                ${monster.difficulty}
+                ${monster.level}
             </p>
+
             <p>
-                ${monster.description}
+                ${monster.detail}
             </p>
+
         </div>
+
     `;
-    //모달의 숨김 상태 제거
+
+    /* hidden 제거 */
     modal.classList.remove("hidden");
+
     const closeBtn = modal.querySelector(".close-btn");
-    //닫힘 버튼 클릭시 모달 닫힘 실행
+
+    /* X 버튼 클릭 시 닫기 */
     closeBtn.addEventListener("click", () => {
         closeModal();
     });
 }
+
+// 모달 닫기
+
 function closeModal() {
     const modal = document.getElementById("wikiModal");
-    //모달에 숨김 상태 추가
+
     modal.classList.add("hidden");
 }
+// 모달 바깥 클릭 시 닫기
+
+const modal = document.getElementById("wikiModal");
+
+modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        closeModal();
+    }
+});
