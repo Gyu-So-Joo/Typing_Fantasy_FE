09f6c60a1e8cceaa1f6576c9838a5e7899d2a237
kimspace00 <kara6614@gmail.com>
2026-05-27 16:28:32 +0900

    Fix: CSS 수정

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,17 @@
+// 타이핑 효과
+const content = "코드를 타이핑하여 몬스터를 잡아보세요!";
+const text = document.querySelector(".text");
+let i = 0;
+
+function typing() {
+  if (i < content.length) {
+    let txt = content.charAt(i);
+    text.innerHTML += txt;
+    i++;
+  }
+}
+setInterval(typing, 100);
+
 // 게임 시작 버튼 클릭
 function startGame() {
   // localStorage 에 저장된 로그인 회원 번호 가져오기
