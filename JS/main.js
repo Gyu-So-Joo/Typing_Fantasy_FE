b5787cdd0ee9b6d4706c51ec25795a65028ef2c8
SO-SO <sys107502@gmail.com>
2026-05-28 16:04:17 +0900

    Fix: main.js에서 명칭 통일 문제로 인한 오류 해결

--- JS/main.js
+++ JS/main.js
@@ -4,26 +4,26 @@ const text = document.querySelector(".text");
 let i = 0;
 
 function typing() {
-  if (i < content.length) {
-    let txt = content.charAt(i);
-    text.innerHTML += txt;
-    i++;
-  }
+    if (i < content.length) {
+        let txt = content.charAt(i);
+        text.innerHTML += txt;
+        i++;
+    }
 }
 setInterval(typing, 100);
 
 // 게임 시작 버튼 클릭
 function startGame() {
-  // localStorage 에 저장된 로그인 회원 번호 가져오기
-  const memberId = localStorage.getItem("loginMemberId");
+    // localStorage 에 저장된 로그인 회원 번호 가져오기
+    const memberId = localStorage.getItem("loginUser");
 
-  // 로그인 안 된 상태
-  if (!memberId) {
-    alert("로그인이 필요합니다.");
-    location.href = "login.html";
-    return;
-  }
+    // 로그인 안 된 상태
+    if (!memberId) {
+        alert("로그인이 필요합니다.");
+        location.href = "login.html";
+        return;
+    }
 
-  // 로그인 된 상태
-  location.href = "language.html";
+    // 로그인 된 상태
+    location.href = "language.html";
 }
