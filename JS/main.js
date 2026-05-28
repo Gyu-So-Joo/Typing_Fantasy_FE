eb92922b3eca07bf29e1e03e9e8e0b15a832c403
SO-SO <sys107502@gmail.com>
2026-05-28 17:17:33 +0900

    Feat: checkLogin.js 를 만들어 페이지에 들어갈때 마다 로그인 여부 확인 및 조치

--- JS/main.js
+++ JS/main.js
@@ -14,17 +14,6 @@ setInterval(typing, 100);
 
 // 게임 시작 버튼 클릭
 function startGame() {
-    // localStorage 에 저장된 로그인 회원 번호 가져오기
-    const memberId = localStorage.getItem("loginUser");
-
-    // 로그인 안 된 상태
-    if (!memberId) {
-        alert("로그인이 필요합니다.");
-        location.href = "login.html";
-        return;
-    }
-
-    // 로그인 된 상태
     location.href = "language.html";
 }
 
