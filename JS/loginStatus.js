84066b38f670fa1d947ef9b2ccdc2df97578100e
kimspace00 <kara6614@gmail.com>
2026-06-02 09:28:13 +0900

    Feat: loginStatus 로직 코드 수정

--- JS/loginStatus.js
+++ JS/loginStatus.js
@@ -1,7 +1,9 @@
 console.log("loginStatus 실행");
-// 로그인 상태시 이미지 로고 변경 로그인->로그아웃
+
+// 로그인 여부 확인
 const memberId = localStorage.getItem("loginUser");
 
-if (memberId) {
-  document.querySelector(".login-logo").src = "img/logout.png";
-}
+// 로그인 상태시 이미지 로고 변경 로그인->로그아웃
+document.querySelector(".login-logo").src = memberId
+  ? "img/logout.png"
+  : "img/login.png";
