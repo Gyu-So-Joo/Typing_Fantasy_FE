9064172f7df3058920e7ce7a1c1f5ecd7fc83016
kimspace00 <kara6614@gmail.com>
2026-06-02 11:28:42 +0900

    Refactor: Refactor: 환경변수 추가에 따른 회원가입 처리 변경

---
+++ src/js/register.js
@@ -0,0 +1,51 @@
+// 회원가입 JS
+const AUTH_API = import.meta.env.VITE_API_URL;
+
+// 메시지 출력
+function showMsg(id, text, ok) {
+  const el = document.getElementById(id);
+
+  el.textContent = text;
+  el.className = "msg " + (ok ? "ok" : "fail");
+}
+
+// 회원가입
+const registerBtn = document.getElementById("registerBtn");
+
+registerBtn.addEventListener("click", () => {
+  const id = document.getElementById("regId").value.trim();
+  const pw = document.getElementById("regPw").value.trim();
+
+  if (!id || !pw) {
+    showMsg("registerMsg", "모든 항목을 입력해주세요", false);
+    return;
+  }
+
+  fetch(`${AUTH_API}/user/register`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      name: id,
+      password: pw,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.status === 201) {
+        // showMsg("registerMsg", "가입 완료! 로그인 페이지로 이동합니다.", true);
+
+        setTimeout(() => {
+          location.href = "login.html";
+        }, 1500);
+      } else {
+        showMsg(
+          "registerMsg",
+          data.message || "가입 실패. 아이디를 확인해주세요.",
+          false,
+        );
+      }
+    })
+    .catch(() => {
+      showMsg("registerMsg", "서버 오류", false);
+    });
+});
