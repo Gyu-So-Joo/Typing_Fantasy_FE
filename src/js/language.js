2097743a9ee05aef56ae99fd63d794d0b0e5ef63
SO-SO <sys107502@gmail.com>
2026-06-02 11:25:02 +0900

    Fix: 언어 선택 이벤트 수정

--- src/js/language.js
+++ src/js/language.js
@@ -1,6 +1,7 @@
 // 언어 선택 JS
 
 // 사용 언어 선택 타이핑 효과
+
 const content = "사용 언어를 선택하세요";
 const text = document.querySelector(".select-text");
 let i = 0;
@@ -15,22 +16,17 @@ function typing() {
 setInterval(typing, 100);
 
 // 언어 선택
-function selectLanguage(language) {
-    // 선택 언어 저장
-    localStorage.setItem("selectedLang", language);
 
-    const javaBtn = document.getElementById("javaBtn");
-    const jsBtn = document.getElementById("jsBtn");
-    javaBtn.addEventListener("click", () => {
-        console.log("선택 언어:", language);
-
-        // 맵 선택 페이지 이동
-        location.href = "map.html";
-    });
-    jsBtn.addEventListener("click", () => {
-        console.log("선택 언어:", language);
-
-        // 맵 선택 페이지 이동
-        location.href = "map.html";
-    });
-}
+// 선택 언어 저장
+const javaBtn = document.getElementById("javaBtn");
+const jsBtn = document.getElementById("jsBtn");
+javaBtn.addEventListener("click", () => {
+    localStorage.setItem("selectedLang", "JAVA");
+    // 맵 선택 페이지 이동
+    location.href = "map.html";
+});
+jsBtn.addEventListener("click", () => {
+    localStorage.setItem("selectedLang", "JS");
+    // 맵 선택 페이지 이동
+    location.href = "map.html";
+});
